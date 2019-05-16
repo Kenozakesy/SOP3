87d70e6e463a098d03e98eb0fca6ff0a89fdc018
Kenozakesy <thezemmer@gmail.com>
2019-05-16 14:20:06 +0200

    groovy file update

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,6 +1,11 @@
 pipeline {
     agent { docker { image 'maven:3.3.3' } }
     stages {
+        stage('Ok') {
+            steps {
+                echo "OK"
+            }
+        }
         stage('build') {
             steps {
                 sh 'mvn --version'
